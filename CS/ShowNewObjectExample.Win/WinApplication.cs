5f0570851538ead3ca29aa3e14ac8f189030bff9
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-05-23 16:29:48 +0300

    initial commit

---
+++ CS/ShowNewObjectExample.Win/WinApplication.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using DevExpress.ExpressApp.Win;
+using DevExpress.ExpressApp.Updating;
+using DevExpress.ExpressApp;
+
+namespace ShowNewObjectExample.Win {
+    public partial class ShowNewObjectExampleWindowsFormsApplication : WinApplication {
+        public ShowNewObjectExampleWindowsFormsApplication() {
+            InitializeComponent();
+        }
+        private void ShowNewObjectExampleWindowsFormsApplication_DatabaseVersionMismatch(object sender, DevExpress.ExpressApp.DatabaseVersionMismatchEventArgs e) {
+			e.Updater.Update();
+			e.Handled = true;
+        }
+    }
+}
