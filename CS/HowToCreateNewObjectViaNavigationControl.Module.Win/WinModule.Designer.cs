676883ad92fc13c23e286b72550504ae37de101b
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-05-23 16:29:48 +0300

    initial commit

---
+++ CS/HowToCreateNewObjectViaNavigationControl.Module.Win/WinModule.Designer.cs
@@ -0,0 +1,36 @@
+namespace HowToCreateNewObjectViaNavigationControl.Module.Win {
+   partial class HowToCreateNewObjectViaNavigationControlWindowsFormsModule {
+      /// <summary> 
+      /// Required designer variable.
+      /// </summary>
+      private System.ComponentModel.IContainer components = null;
+
+      /// <summary> 
+      /// Clean up any resources being used.
+      /// </summary>
+      /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+      protected override void Dispose(bool disposing) {
+         if (disposing && (components != null)) {
+            components.Dispose();
+         }
+         base.Dispose(disposing);
+      }
+
+      #region Component Designer generated code
+
+      /// <summary> 
+      /// Required method for Designer support - do not modify 
+      /// the contents of this method with the code editor.
+      /// </summary>
+      private void InitializeComponent() {
+         // 
+         // HowToCreateNewObjectViaNavigationControlWindowsFormsModule
+         // 
+         this.RequiredModuleTypes.Add(typeof(HowToCreateNewObjectViaNavigationControl.Module.HowToCreateNewObjectViaNavigationControlModule));
+         this.RequiredModuleTypes.Add(typeof(DevExpress.ExpressApp.Win.SystemModule.SystemWindowsFormsModule));
+
+      }
+
+      #endregion
+   }
+}
